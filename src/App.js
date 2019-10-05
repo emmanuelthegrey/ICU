d395207cdb1ddea02e28cb39f94d19c91bed119d
emmanuelthegrey <whoisemmanuel@gmail.com>
2019-10-05 18:44:09 -0400

    landing page form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Navigation from './components/navigation/navigation';
+import Logo from './components/logo/logo';
+import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 		<Navigation />
-		{/* <Logo />
+		 <Logo />
 		<ImageLinkForm />
-		<FaceRecgonition /> */}
+		{/*<FaceRecgonition /> */}
     </div>
   );
 }
