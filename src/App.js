b4b3ddd3b34b07fd8a479252ef8a9163ce1b94e0
emmanuelthegrey <whoisemmanuel@gmail.com>
2019-10-06 16:13:10 -0400

    face recgonition api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,89 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import Rank from './components/rank/rank';
 import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
+import SignIn from './components/signin/signin';
+import FaceRecgonition from './components/faceRecgonition/faceRecgonition';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-		<Navigation />
-		 <Logo />
-		 <Rank />
-		<ImageLinkForm />
-		{/*<FaceRecgonition /> */}
-    </div>
-  );
+const app = new Clarifai.App({
+	apiKey: '2e160a4e9ef1423ba0d8f58490998775'
+});
+
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 70,
+			density: {
+				enable: true,
+				value_area: 500
+			}
+		}
+	}
+}
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			input: '',
+			imageUrl: '',
+			box: {},
+		}
+	}
+
+	calculateFaceLocation = (data) => {
+		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+		const image = document.getElementById('inputimage');
+		const width = Number(image.width);
+		const heigt = Number(image.height);
+
+		return {
+			leftCol: clarifaiFace.left_col * width,
+			topRow: clarifaiFace.top_row * heigt,
+			rightCol: width - (clarifaiFace.right_col * width),
+			bottomRow: heigt - (clarifaiFace.bottom_row * heigt)
+		}
+	}
+
+	displayFaceBox = (box) => {
+		console.log(box);
+		this.setState({box: box});
+	}
+
+	onInputChange = (event) => {
+		this.setState({ input: event.target.value })
+	}
+
+	onSubmit = () => {
+		this.setState({ imageUrl: this.state.input })
+		app.models
+			.predict(
+				Clarifai.FACE_DETECT_MODEL,
+				this.state.input)
+			.then(response => this.displayFaceBox( this.calculateFaceLocation(response)))
+				.catch(err => console.log(err));
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Navigation />
+				<SignIn />
+				<Particles className='particles'
+					params={particlesOptions}
+				/>
+				<Logo />
+				<Rank />
+				<ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
+				<FaceRecgonition box={this.state.box} imageUrl={this.state.imageUrl} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/imageLinkForm/imageLinkForm.js
+++ src/components/imageLinkForm/imageLinkForm.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './imagelinkform.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onSubmit}) => {
 	return (
 		<div>
 			<p className='f3'>
-				{'I never forget a face. Try me'}
+				{'I never forget a face. Try me.'}
 			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-70 center' type='tex' />
-					<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Scan</button>
+					<input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+					<button onClick={onSubmit} className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Scan</button>
 				</div>
 			</div>
 		</div>
